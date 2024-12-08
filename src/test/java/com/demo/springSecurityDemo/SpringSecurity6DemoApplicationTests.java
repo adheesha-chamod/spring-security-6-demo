1db7b63e063bc4b1625f88bece990c1f7dea6895
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-12-08 18:25:52 +0530

    init commit

---
+++ src/test/java/com/demo/springSecurityDemo/SpringSecurity6DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springSecurityDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
