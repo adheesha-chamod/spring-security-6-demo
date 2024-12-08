c4a980ce29239bab6fa0e5d46d8dd1c7e6bfeec2
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-12-09 01:45:03 +0530

    Add password encoder bean

--- src/main/java/com/demo/springSecurityDemo/config/WebSecurityConfig.java
+++ src/main/java/com/demo/springSecurityDemo/config/WebSecurityConfig.java
@@ -9,11 +9,9 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -57,10 +55,15 @@ public class WebSecurityConfig {
 //    }
 
     @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();     // default strength -> 10 | max strength -> 31 | min strength -> 4
+    }
+
+    @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
 }
