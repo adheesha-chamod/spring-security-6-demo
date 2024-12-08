b7802540e4cd810ebe8b0ad7ec69cf3a1c048d05
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-12-09 01:31:18 +0530

    Add files

---
+++ src/main/java/com/demo/springSecurityDemo/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.demo.springSecurityDemo.service;
+
+import com.demo.springSecurityDemo.entity.User;
+import com.demo.springSecurityDemo.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isEmpty()) {
+            log.warn("[loadUserByUsername] User '{}' not found", username);
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new CustomUserDetails(user.get());
+    }
+}
